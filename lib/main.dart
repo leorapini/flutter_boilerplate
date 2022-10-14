6bc4845c59b0ae487cf7277cd01a0abaf009e77c
Leo Rapini <leorapini@gmail.com>
2022-10-14 10:36:17 -0300

    add homepage

--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,8 @@
-import 'package:flutter_boiler/presentation/theme/theme.dart';
+import 'package:flutter/material.dart';
 
 import 'injection.dart' as injection;
-import 'package:flutter/material.dart';
-import 'package:flutter_bloc/flutter_bloc.dart';
+import 'presentation/pages/home.dart';
+import 'presentation/theme/theme.dart';
 
 void main() {
   WidgetsFlutterBinding.ensureInitialized();
@@ -15,18 +15,17 @@ class App extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return  MaterialApp(
-        title: 'Space Pics',
-        theme: ThemeData(
-          primarySwatch: Colors.blue,
-          textTheme: textThemeDefault,
-          fontFamily: fontFamily,
-        ),
-        home: const Scaffold(
-            body: Center(
-          child: Text('Hey'),
-        )),
-      );
-
+    return MaterialApp(
+      title: 'Space Pics',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+        textTheme: textThemeDefault,
+        fontFamily: fontFamily,
+      ),
+      home: const Scaffold(
+          body: Center(
+        child: HomePage(),
+      )),
+    );
   }
 }
